eabb45535ac51654b643a0727d1df2f56655008c
JustineSGH <justine.sgherzi@etud.u-picardie.fr>
2017-04-25 15:42:24 +0200

    Correction d'une regex

--- crawler.php
+++ crawler.php
@@ -21,30 +21,68 @@ function crawl($url){
     $contenu = file_get_contents('contenu_page.txt');
 
     preg_match_all('#(<span class="final-price" data-cerberus="txt_plp_discountedPrice1"> <span itemprop="price">(.*)</span>(.*)</span>|<strong class="final-price" data-cerberus="txt_plpProduit_discountedPrice1">(.*)</strong>)#', $contenu, $prix);
-    preg_match_all('#(<div class="title"><h2 data-cerberus="txt_pdp_productName1" itemprop="name">(.+)</h2></div>|<div class="title" data-cerberus="lnk_plpProduit_productName1">(.+)</div>)#', $contenu, $nom);
-
-    if(file_exists('fichier_mails.txt')){
-      unlink('fichier_mails.txt');
-    }
-    $fp_fichier_emails = fopen('fichier_mails.txt', 'a');
+    preg_match_all('#(<h2 data-cerberus="txt_pdp_productName1" itemprop="name">(.+)</h2>|<div class="title" data-cerberus="lnk_plpProduit_productName1">(.+)</div>)#', $contenu, $nom);
+  
+    $nom[0] = preg_replace('#<h2 data-cerberus="txt_pdp_productName1" itemprop="name">#',  '', $nom[0]);
+    $nom[0] = preg_replace('#</h2>#', '', $nom[0]);
 
     echo "<table class='table table-bordered'>
         <tr>
           <th>Nom</th>
           <th>Prix</th>
         </tr>";
-    for($i=0; $i< count($prix[0]); $i++){
-      echo"
-      <tr> 
-        <th>"; echo $nom[0][$i]; echo"</th>
-        <th>"; echo $prix[0][$i]; echo"</th>
-      </tr>";
-    }
+    	for($i=0; $i< count($prix[0]); $i++){
+      	echo"
+      	<tr> 
+        	<th>";
+        			echo $nom[0][$i]; echo"</th>
+        	<th>"; echo $prix[0][$i]; echo"</th>
+      	</tr>";
+    	}
     echo" </table>";
 
-    fclose($fp_fichier_emails);
+
+    //extraction des liens 
+   /* preg_match_all('#"/?[a-zA-Z0-9_./-]+.(php|html|htm|aspx)"#', $contenu, $liens_extraits);
+    if(file_exists('liens.txt')){
+    	 $fichier_liens = fopen('liens.txt', 'a');
+
+    	foreach ($liens_extraits[0] as $element) {
+    		$fichier_liens = fopen('liens.txt', 'a');
+    		$gestion_doublons = file_get_contents('liens.txt');
+    		$element = preg_replace('#"#', '', $element);
+    		$follow_url = $element;
+    		$pattern = '#'.$follow_url.'#';
+    		if (!preg_match($pattern, $gestion_doublons)) {
+          		fputs($fichier_liens, $follow_url);
+      		}
+    	}
+    }
+    else {
+    	$fp_fichier_liens = fopen('liens.txt', 'a');
+
+	    foreach ($liens_extraits[0] as $element) {
+	        $element = preg_replace('#"#', '', $element);
+	        $follow_url = $element;
+	        fputs($fichier_liens, $follow_url);
+	    }
+  	}
+  fclose($fichier_liens);*/
 }
 crawl($url);
+
+/*$lire_autres_pages = fopen('liens.txt', 'r');
+		
+$numero_de_ligne = 1;
+
+while(!feof($lire_autres_pages)) {
+  $page_suivante = $url;
+  $page_suivante .= fgets($lire_autres_pages);
+  echo $numero_de_ligne . ' Analyses en cours, page : ' .  $page_suivante . "\n";
+  $numero_de_ligne++;
+  crawl($page_suivante);
+}
+fclose ($lire_autres_pages);*/
 
 ?>
 
